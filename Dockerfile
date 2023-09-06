041e91c8c80c5af0c0bf42ff2f75fab308e9a824
TodayGlamourIs <iutvoo0936@gmail.com>
2023-09-06 11:09:36 +0800

    導入linebot介面及說明更新

--- Dockerfile
+++ Dockerfile
@@ -11,11 +11,11 @@ COPY densenetD14.h5 /app/
 # 复制两个文件夹及其内容到工作目录
 COPY static /app/static
 COPY templates /app/templates
+COPY requirements.txt .
+COPY config.ini .
 
 # 安装依赖项
-RUN python -m pip install --upgrade pip
-RUN apt-get update && apt-get install -y libgl1-mesa-glx
-RUN pip install pymongo flask pydicom keras opencv-python numpy Pillow line-bot-sdk tensorflow
+RUN pip install --no-cache-dir -r requirements.txt
 
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # 初始化資料庫連線
 from pymongo.mongo_client import MongoClient
-url = "mongodb+srv://********@cluster0.aagcvdi.mongodb.net/?retryWrites=true&w=majority"
+url = "mongodb+srv://*******@cluster0.aagcvdi.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(url)
 # try:
 #     client.admin.command('ping')
@@ -20,12 +20,16 @@ import base64
 from io import BytesIO
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage,QuickReply, QuickReplyButton, MessageAction, ImageSendMessage
 import time
 import logging
+import configparser
 
 app = Flask(__name__, static_folder="static", static_url_path="/" )
 app.secret_key="LoginTest"
+config = configparser.ConfigParser()
+config.read('config.ini')
+
 # 设置日志记录
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -200,8 +204,8 @@ def preprocess_png_image(png_path):
 
 
 # 初始化 LINE Bot API 和 WebhookHandler
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 @app.route("/line_webhook", methods=['POST'])
 def line_webhook():
@@ -217,6 +221,41 @@ def line_webhook():
         return 'Invalid signature. Please check your channel access token/channel secret.', 400
 
     return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_image_message(event):
+    if event.message.text == "使用說明":
+        reply_msg = "請上傳X光片的圖片檔，系統會依據您提供的圖片進行判斷，並回覆您肺部病症預測結果"
+    elif event.message.text == "我們的網站":
+        reply_msg = "Http..........."
+    elif event.message.text == "聯絡我們":
+        # 建立快速按鈕
+        quick_reply = QuickReply(items=[
+            QuickReplyButton(action=MessageAction(label="組長：楊致", text="Email：lovedad1018@gmail.com")),
+            QuickReplyButton(action=MessageAction(label="組員：陳玥年", text="Email：a829516@gmail.com")),
+            QuickReplyButton(action=MessageAction(label="組員：陳昭宇", text="Email：chaoyu0314@gmail.com")),
+            QuickReplyButton(action=MessageAction(label="組員：周伯儒", text="Email：chou8102@gmail.com")),
+            QuickReplyButton(action=MessageAction(label="組員：粘馨云", text="Email：hsingyun0813@gmail.com")),
+            QuickReplyButton(action=MessageAction(label="組員：黃柏章", text="Email：iutvoo0936@gmail.com"))
+        ])
+        reply_msg = "請選擇要聯絡的組員"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg, quick_reply=quick_reply))
+    elif"@gmail.com" in event.message.text:
+        if event.message.text == "Email：lovedad1018@gmail.com":
+            reply_msg="履歷參考：https://drive.google.com/drive/shared-with-me"                                                     
+        elif event.message.text == "Email：a829516@gmail.com":
+            reply_msg="履歷參考：https://pda.104.com.tw/profile/share/dhX2z6rGCOBjyeGBX2aEPMFBMsT7A7wM"                                                     
+        elif event.message.text == "Email：chaoyu0314@gmail.com":
+            reply_msg="履歷參考："                                                     
+       
+        elif event.message.text == "Email：hsingyun0813@gmail.com":
+            reply_msg="履歷參考：https://drive.google.com/drive/u/0/folders/1gVXNnEwXMNueSlJAs7DS_s4ybJFXLd66"
+        elif event.message.text == "Email：iutvoo0936@gmail.com":
+            reply_msg="履歷參考："
+    else:
+        reply_msg = "請上傳圖片檔"
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
@@ -235,13 +274,14 @@ def handle_image(event):
     # 根據預測結果生成回覆
     reply_msg = f"""
     預測結果:
-    其他: {(prediction[0][0] * 100):.2f}%
-    肺炎: {(prediction[0][1] * 100):.2f}%
-    肺水腫: {(prediction[0][2] * 100):.2f}%
-    肺不張: {(prediction[0][3] * 100):.2f}%
+    其他病症可能性: {(prediction[0][0] * 100):.2f}%
+    肺炎可能性: {(prediction[0][1] * 100):.2f}%
+    肺水腫可能性: {(prediction[0][2] * 100):.2f}%
+    肺不張可能性: {(prediction[0][3] * 100):.2f}%
+    結果僅基於X光片的分析，並不代表最終診斷。建議您儘早與專業醫生進行進一步診斷和討論，以確定您的肺部健康狀況
     """
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8070)
+if __name__ == "__main__":  
+    app.run(host='0.0.0.0', port=8001)
